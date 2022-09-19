cea770b851ee71eb1b3cfc4397c08c24ba5c7f9a
venusgalstar <venusgalstar@gmail.com>
2022-09-20 01:16:03 +0900

    remove some files

--- core/interfaces/IERC20.sol
+++ core/interfaces/IERC20.sol
@@ -1,11 +1,9 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.5.0;
 
 interface IERC20 {
-    // event Approval(address indexed owner, address indexed spender, uint value);
-    // event Transfer(address indexed from, address indexed to, uint value);
 
-    function name() external view returns (string memory);
+   function name() external view returns (string memory);
     function symbol() external view returns (string memory);
     function decimals() external view returns (uint8);
     function totalSupply() external view returns (uint);
