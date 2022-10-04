9d175befdb9827699ccae5fd4571210c2ebfb717
CryptoSnowPrince <topdirector2017@gmail.com>
2022-10-04 02:59:14 +0000

    rename as periphery

---
+++ periphery/hardhat.config.ts
@@ -0,0 +1,21 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+    ]
+  },
+  defaultNetwork: "hardhat",
+};
+
+export default config;
